19db1c1e71910d8e6070ac76861c2af42a2fc9b4
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-07-15 11:11:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Polygon {
+  constructor(numbers) {
+    this.numbers = numbers;
+  }
+  get countSides() {
+    return this.numbers.length;
+  }
+  get perimeter() {
+    let sum = 0;
+    for (let i = 0; i < this.numbers.length; i++) {
+      sum = sum += this.numbers[i];
+    }
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.numbers.length !== 3) {
+      return false;
+    }
+
+    const [side1, side2, side3] = this.numbers;
+
+    return (
+      side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2
+    );
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.numbers.length !== 4) {
+      return false;
+    }
+
+    const [side1, side2, side3, side4] = this.numbers;
+
+    return side1 === side2 && side1 === side3 && side1 === side4;
+  }
+
+  get area() {
+    const [side1, side2, side3, side4] = this.numbers;
+    return side1 * side2;
+  }
+}
